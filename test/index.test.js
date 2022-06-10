5373fbc780fbe50acbb81a8d8449dd6b8a51b0bf
Joel Edwards <joeledwards@gmail.com>
2022-06-10 07:48:33 -0600

    chore: upgrade @buzuli/unhandled; test passing config to unhandled

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,6 @@
 const tap = require('tap')
 const app = require('../lib')
+const EventListener = require('events')
 
 tap.test('app should invoke an entry point', async assert => {
   let didRun = false
@@ -24,7 +25,6 @@ tap.test('options and entry may be swapped', async assert => {
 tap.test('app permit currying with options first', async assert => {
   ;await (async () => {
     let didRun = false
-    console.info('Try curry!')
     await app({})(async () => { didRun = true })
     assert.equal(didRun, true)
   })()
@@ -62,4 +62,38 @@ tap.test('app should resolve and forward injected modules', async assert => {
   assert.equal(resolved.modules.a, 'a')
   assert.equal(resolved.modules.b, 'b')
   assert.equal(resolved.modules.c, 'c')
+})
+
+tap.test('app should forward options to unhandled', async assert => {
+  let handled
+  let exited = false
+
+  const done = {}
+  done.promise = new Promise(resolve => {
+    done.halt = () => resolve()
+  })
+
+  const events = new EventListener()
+  const handle = {
+    exit: true,
+    handler: ({ event }) => {
+      handled = event
+      done.halt()
+    },
+    context: {
+      events,
+      exit: () => {
+        exited = true
+      }
+    }
+  }
+
+  await app({ handle })(async () => {
+    console.info("In app")
+    events.emit('SIGINT')
+    await done.promise
+  })
+
+  assert.equal(handled, 'SIGINT')
+  assert.true(exited)
 })
